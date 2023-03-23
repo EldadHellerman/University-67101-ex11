1aa369437ab9d2564d2c89293ff2283fd22261ba
hellermaneldad <99619770+hellermaneldad@users.noreply.github.com>
2023-03-23 19:30:34 +0200

    init

---
+++ src/temp.py
@@ -0,0 +1,12 @@
+#################################################################
+# FILE : XXX XXX XXX XXX
+# WRITER : Eldad Hellerman , hellerman , 322898552
+# EXERCISE : intro2cs2 ex? XXX XXX XXX XXX 2023
+# DESCRIPTION: XXX XXX XXX XXX
+# STUDENTS I DISCUSSED THE EXERCISE WITH: none.
+# WEB PAGES I USED: none.
+# NOTES: none.
+#################################################################
+
+if __name__ == "__main__":
+    pass
