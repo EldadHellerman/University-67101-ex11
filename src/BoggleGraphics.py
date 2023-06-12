7942fd3b4d5cbcd091fa1f59dcb2ec36cf3932d7
EldadHellerman <EldadHellerman@gmail.com>
2023-06-12 12:00:40 +0300

    main graphics backend.

--- src/BoggleGraphics.py
+++ src/BoggleGraphics.py
@@ -1,73 +1,132 @@
-import tkinter as tk
+from tkinter import *
 
 class BoggleGraphics:
-        
-    def button_1_pressed(self):
-        print("hello world!")
-    
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.geometry("500x500")
-        self.root.title("Boggle")
-        
-        self.root.focus_set()
-        self.root.bind("<Escape>", lambda e: e.widget.quit())
-        self.root.bind("1", lambda e: self.label.configure(text="isdugihdbgjhd"))
 
-        frame = tk.Frame(self.root, background="green", borderwidth=20)
-        frame.pack()
+    def __init__(self): #### TODO finish graphical layout of button and canvas
+        font_labels_and_buttons = ("Arial", 20)
+        font_words_found = ("Arial", 16)
+        theme_bg = "#202020"
+        theme_text = "#f0f0f0"
+        theme_highlight = "#ff0000"
+
+        root = Tk()
+        root.title("Boggle")
+        root.geometry("600x600")
+        root.minsize(500, 500)
+        root.configure(bg=theme_bg)
+        root.bind("<Escape>", lambda e: e.widget.quit())
+        root.grid_columnconfigure(0, weight=2, minsize=400)
+        root.grid_columnconfigure(1, weight=1, minsize=200)
+        root.grid_rowconfigure(0, weight=1, minsize=200)
+        root.grid_rowconfigure(1, weight=2, minsize=400)
+
+        frame_top = Frame(root, bg="red")
+        frame_side = Frame(root, bg="green")
+        frame_main = Frame(root, bg="purple")
+        frame_top.grid(column=0, row=0, columnspan=2, sticky="nsew")
+        frame_side.grid(column=1, row=0, rowspan=2, sticky="nsew")
+        frame_main.grid(column=0, row=1, sticky="nsew")
+
         
-        leftframe = tk.Frame(self.root, background="purple")
-        leftframe.pack(side=tk.LEFT)
+        Label(frame_side, text="Words found:", font=font_labels_and_buttons, bg=theme_bg, fg=theme_text).pack(side=TOP, fill=X)
+        # scrollbar_word_found = Scrollbar(frame_side, bg=theme_bg)
+        # scrollbar_word_found.pack(side = RIGHT, fill = BOTH)
+        listbox_words_found = Listbox(frame_side, font=font_words_found, bg=theme_bg, fg=theme_text,
+                                      highlightthickness=0, state="disabled", selectmode=SINGLE)
+        listbox_words_found.pack(fill=BOTH, expand=True)
+        # listbox_words_found.config(yscrollcommand = scrollbar_word_found.set)
+        # scrollbar_word_found.config(command = listbox_words_found.yview)
+        
+        label_time = Label(frame_top, text = "Time:", font=font_labels_and_buttons)
+        label_time.pack(side=RIGHT)
+        
+        label_score = Label(frame_top, text = "Score:", font=font_labels_and_buttons)
+        label_score.pack()
 
-        # self.label = tk.Label(frame, text = "Hello world")
-        # self.label.pack()
-        self.counter = 0
-        self.label_score = tk.Label(frame, text = "Hello world")
-        self.label_score.pack()
+        button_reset = Button(frame_top, text = "Reset")
+        button_reset.pack(side=LEFT)
+        
+        button_submit = Button(frame_top, text = "Submit")
+        button_submit.pack(side=LEFT)
 
-        self.button_reset_or_startover = tk.Button(leftframe, text = "left_frame_1", command = self.button_1_pressed)
-        self.button_reset_or_startover.pack(padx = 3, pady = 3)
+        label_input = Label(frame_top, text = "input word")
+        label_input.pack(side=LEFT)
+
+        canvas = Canvas(frame_main, bg=theme_bg)
+        canvas.grid(column=0, row=1, sticky="nsew")
+
+        #export all needed widgets:
+        self.label_time = label_time
+        self.label_score = label_score
+        self.button_reset = button_reset
+        self.button_submit = button_submit
+        self.label_input = label_input
+        self.listbox_words_found = listbox_words_found
+        self.canvas = canvas
+        self.root = root
     
     def start(self):
         self.root.mainloop()
-
-    def button_1_pressed(self):
-        self.counter += 1
-        self.label.config(text= f"I was clicked {self.counter} times")
     
-
-    def set_time(self, updated_time):
-        self.time = updated_time
+    def display_board(self): ### TODO write
+        #display a boggle board on a canvas
         pass
 
+    def words_found_enable(self, enabled: bool):
+            self.listbox_words_found.configure(state=NORMAL if enabled else DISABLED)
+    
+    def words_found_add(self, string: str):
+            state = self.listbox_words_found.cget("state")
+            self.words_found_enable(True)
+            self.listbox_words_found.insert(END, string)
+            self.words_found_enable(state=="normal")
+    
+    def words_found_clear(self):
+            state = self.listbox_words_found.cget("state")
+            self.words_found_enable(True)
+            self.listbox_words_found.delete(0, END)
+            self.words_found_enable(state=="normal")
 
-    def set_score(self, score_value: int):
-        self.label_score.config(text= str(score_value))
-        
-    def set_words(self, words_list: list):
-        self.words = words_list
-        pass
-        
+    def set_time(self, time: str):
+        self.label_time.config(text=time)
+
+    def set_score(self, score: str):
+        self.label_score.config(text=score)
+    
+    def set_input(self, string: str):
+        self.label_input.config(text=string)
+    
     def set_reset_or_startover(self, reset_or_startover):
-        self.button_reset_or_startover.configure(
-            text = ("Reset Game" if reset_or_startover else "Startover"))
-        
-    
-    def set_button_reset_or_startover_clicked_function(self, func):
-        self.button_reset_or_startover.configure(command=func)
+        self.button_reset.configure(text = ("Reset" if reset_or_startover else "Startover"))
 
-    
+    def set_guess_text(self, note :str): ##################TODO delete this function
+        """_summary_
 
-    def set_guess_text(note):
+        Args:
+            note (str): _description_
+        """     
         """ param: a single note:str """
         self.guess_text = note
         pass
         # אני לא מבין את הכתב שלך ולא מצליח לקרוא את התמונה!! וגם לא זוכר את הפונקציות שלנו
+        #😂
+        #שים לב לסטנדרט שבו דוקסטרינג לפונקציות נכתב!
+        # אבל עדיף להשאיר את זה לסוף
     
+    def set_cb_button_reset(self, func):
+        self.button_reset.configure(command=func)
     
-    def draw():
+    def set_cb_button_submit(self, func):
+        self.button_submit.configure(command=func)
+    
+    def set_cb_board(self, func): ##########TODO not finished
         pass
+        # another function which translate canvas clicks into coordinants is needed.
+        # self.canvas.configure(command=func)
+    
+    def set_cb_word_selected(self, func):
+        self.listbox_words_found.bind("<<ListboxSelect>>", lambda e: func(self.listbox_words_found.curselection()))
 
 if __name__ == "__main__":
-    ksdjvnjs = BoggleGraphics()
+    g = BoggleGraphics()
+    g.start()
--- src/boggle.py
+++ src/boggle.py
@@ -1,14 +1,25 @@
 #################################################################
 # FILE : boggle.py
+# WRITERS : Eldad Hellerman , hellerman , 322898552
+#           Omri Baum, omribaum, 315216853
+# EXERCISE : intro2cs2 ex10 2023
+# DESCRIPTION: A class that is the backend of a snake game.
+# STUDENTS WE DISCUSSED THE EXERCISE WITH: none.
+# WEB PAGES WE USED: none.
+# NOTES: none.
 #################################################################
-#https://prod.liveshare.vsengsaas.visualstudio.com/join?94A6EC3EB4731E7321EA50F0AD9212A046A6
+
+# https://prod.liveshare.vsengsaas.visualstudio.com/join?94A6EC3EB4731E7321EA50F0AD9212A046A6
+
+# https://www.pythonguis.com/faq/pack-place-and-grid-in-tkinter/
+# https://www.pythontutorial.net/tkinter/tkinter-grid/
 
 
 from boggle_board_randomizer import randomize_board
 from BoggleGraphics import BoggleGraphics
 
 
-graphics = None
+graphics: BoggleGraphics = None
 
 def main():
     global graphics
@@ -16,14 +27,35 @@ def main():
 
     board = randomize_board()
     print(board)
-    graphics.set_button_reset_or_startover_clicked_function(cli)
+    graphics.set_cb_button_reset(cb_reset)
+    graphics.set_cb_button_submit(cb_submit)
+    graphics.set_cb_word_selected(cb_word_selected)
+    graphics.set_cb_board(cb_board)
     graphics.set_score(10)
+    graphics.words_found_add("hello!")
+    graphics.words_found_add("hello! snfkj")
+    graphics.words_found_add("bye!")
     # graphics.set_board(board)
     graphics.start()
 
-def cli():
-    print("resdgsdfg")
+def cb_reset():
+    # graphics.words_found_add("byksjdvndksje!")
+    graphics.words_found_clear()
     graphics.set_score(0)
+
+def cb_submit():
+    graphics.set_input("this is a user input that is submited")
+    graphics.set_score(100)
+    graphics.words_found_enable(True)
+
+def cb_word_selected(selection):
+    if(len(selection) != 1): return
+    index = selection[0]
+    print("a word was selected!", index)
+
+def cb_board():
+    print("board was clicked")
+
 
 if __name__ == "__main__":
     main()
